917aa61fd3ac2ef8da031097db1b629a8d17f938
Rohit Chowdary Kundeti <rohitchinna365@gmail.com>
2026-02-13 18:31:02 +0530

    Initial Commit - Basic WAV Music Player

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MusicPlayerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameLbl = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileNameLbl
+            // 
+            this.fileNameLbl.AutoSize = true;
+            this.fileNameLbl.Font = new System.Drawing.Font("Times New Roman", 17.8F);
+            this.fileNameLbl.Location = new System.Drawing.Point(579, 237);
+            this.fileNameLbl.Name = "fileNameLbl";
+            this.fileNameLbl.Size = new System.Drawing.Size(198, 34);
+            this.fileNameLbl.TabIndex = 0;
+            this.fileNameLbl.Text = "No file selected";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(585, 327);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(691, 327);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(639, 78);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 757);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.fileNameLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fileNameLbl;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayerApp
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer soundPlayer = new SoundPlayer();
+        private string filePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "WAV files (*.wav)|*.wav";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+                soundPlayer.SoundLocation = filePath;
+                //fileNameLbl.Text = System.IO.Path.GetFileName(filePath);
+                fileNameLbl.Text = System.IO.Path.GetFileName(filePath);
+            }
+        }
+
+        private void btnPlay_click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                soundPlayer.Play();
+            }
+            else
+            {
+                MessageBox.Show("Please load a music file.");
+            }
+        }
+
+        private void btnStop_click(object sender, EventArgs e)
+        {
+            soundPlayer.Stop();
+        }
+    }
+}
