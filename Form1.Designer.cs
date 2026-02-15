251f50bf797d8e582953f9061f8ed3ce3e63f05e
Rohit Chowdary Kundeti <rohitchinna365@gmail.com>
2026-02-15 23:41:58 +0530

    Load all the music files from a Folder and added play, pause, next, previous functionalities.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.fileNameLbl = new System.Windows.Forms.Label();
             this.btnPlay = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
             this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.lstTracks = new System.Windows.Forms.ListBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // fileNameLbl
-            // 
-            this.fileNameLbl.AutoSize = true;
-            this.fileNameLbl.Font = new System.Drawing.Font("Times New Roman", 17.8F);
-            this.fileNameLbl.Location = new System.Drawing.Point(579, 237);
-            this.fileNameLbl.Name = "fileNameLbl";
-            this.fileNameLbl.Size = new System.Drawing.Size(198, 34);
-            this.fileNameLbl.TabIndex = 0;
-            this.fileNameLbl.Text = "No file selected";
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(585, 327);
+            this.btnPlay.Location = new System.Drawing.Point(579, 405);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(75, 23);
             this.btnPlay.TabIndex = 1;
@@ -54,19 +46,9 @@
             this.btnPlay.UseVisualStyleBackColor = true;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_click);
             // 
-            // btnStop
-            // 
-            this.btnStop.Location = new System.Drawing.Point(691, 327);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
-            this.btnStop.TabIndex = 2;
-            this.btnStop.Text = "Stop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_click);
-            // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(639, 78);
+            this.btnLoad.Location = new System.Drawing.Point(629, 77);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(75, 23);
             this.btnLoad.TabIndex = 3;
@@ -74,28 +56,70 @@
             this.btnLoad.UseVisualStyleBackColor = true;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_click);
             // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(674, 405);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_click);
+            // 
+            // lstTracks
+            // 
+            this.lstTracks.FormattingEnabled = true;
+            this.lstTracks.ItemHeight = 16;
+            this.lstTracks.Location = new System.Drawing.Point(529, 130);
+            this.lstTracks.Name = "lstTracks";
+            this.lstTracks.Size = new System.Drawing.Size(276, 228);
+            this.lstTracks.TabIndex = 5;
+            this.lstTracks.SelectedIndexChanged += new System.EventHandler(this.lstTracks_SelectedIndexChanged);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(418, 234);
+            this.btnPrev.Name = "button1";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 6;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(839, 234);
+            this.btnNext.Name = "button2";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1417, 757);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.lstTracks);
+            this.Controls.Add(this.btnPause);
             this.Controls.Add(this.btnLoad);
-            this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnPlay);
-            this.Controls.Add(this.fileNameLbl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label fileNameLbl;
         private System.Windows.Forms.Button btnPlay;
-        private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.ListBox lstTracks;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace MusicPlayerApp
 {
@@ -15,40 +17,141 @@ namespace MusicPlayerApp
     {
         private SoundPlayer soundPlayer = new SoundPlayer();
         private string filePath = "";
+
+        // NAudio based player
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private List<string> playlist = new List<string>();
+        private int currentTrackIndex = -1;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void playTrack(string filePath)
+        {
+            if(outputDevice != null)
+            {
+                outputDevice.Stop();
+                outputDevice.Dispose();
+                audioFile.Dispose();
+            }
+
+            audioFile = new AudioFileReader(filePath);
+            outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+        }
+
+
         private void btnLoad_click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "WAV files (*.wav)|*.wav";
-
-            if (ofd.ShowDialog() == DialogResult.OK)
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
             {
-                filePath = ofd.FileName;
-                soundPlayer.SoundLocation = filePath;
-                //fileNameLbl.Text = System.IO.Path.GetFileName(filePath);
-                fileNameLbl.Text = System.IO.Path.GetFileName(filePath);
+                if(folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    playlist.Clear();
+                    lstTracks.Items.Clear();
+
+                    string[] files = Directory.GetFiles(folderDialog.SelectedPath);
+
+                    foreach (string file in files)
+                    {
+                        if (file.EndsWith(".mp3") || file.EndsWith(".wav"))
+                        {
+                            playlist.Add(file);
+                            lstTracks.Items.Add(Path.GetFileName(file));
+                        }
+                    }
+
+                    if(playlist.Count > 0)
+                    {
+                        currentTrackIndex = 0;
+                    }
+                }
             }
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = "WAV files (*.wav)|*.wav";
+
+            //if (ofd.ShowDialog() == DialogResult.OK)
+            //{
+            //    filePath = ofd.FileName;
+            //    soundPlayer.SoundLocation = filePath;
+            //    //fileNameLbl.Text = System.IO.Path.GetFileName(filePath);
+            //    fileNameLbl.Text = System.IO.Path.GetFileName(filePath);
+            //}
         }
 
         private void btnPlay_click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(filePath))
+            if (playlist.Count > 0)
             {
-                soundPlayer.Play();
+                //soundPlayer.Play();
+                if (currentTrackIndex < 0)
+                    currentTrackIndex = 0;
+
+                playTrack(playlist[currentTrackIndex]);
             }
             else
             {
-                MessageBox.Show("Please load a music file.");
+                MessageBox.Show("Please load the music files.");
+            }
+        }
+
+        private void btnPause_click(object sender, EventArgs e)
+        {
+            if(outputDevice != null)
+            {
+                if(outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    outputDevice.Pause();
+                }
+                else if(outputDevice.PlaybackState == PlaybackState.Paused)
+                {
+                    outputDevice.Play();
+                }
             }
         }
 
         private void btnStop_click(object sender, EventArgs e)
         {
             soundPlayer.Stop();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (playlist.Count == 0) return;
+
+            currentTrackIndex++;
+
+            if(currentTrackIndex >= playlist.Count)
+            {
+                currentTrackIndex = 0;
+            }
+
+            lstTracks.SelectedIndex = currentTrackIndex;
+            playTrack(playlist[currentTrackIndex]);
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (playlist.Count == 0) return;
+
+            currentTrackIndex--;
+
+            if(currentTrackIndex < 0)
+            {
+                currentTrackIndex = playlist.Count - 1;
+            }
+
+            lstTracks.SelectedIndex = currentTrackIndex;
+            playTrack(playlist[currentTrackIndex]);
+        }
+
+        private void lstTracks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentTrackIndex = lstTracks.SelectedIndex;
         }
     }
 }
